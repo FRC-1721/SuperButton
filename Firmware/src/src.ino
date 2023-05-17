13f9a2b179262aa2439c0736833e8207f5b0e9ef
KenwoodFox <kenwood364@gmail.com>
2023-05-17 15:18:43 -0400

    What a mess

--- Firmware/src/src.ino
+++ Firmware/src/src.ino
@@ -4,15 +4,18 @@
 
 #include <Arduino.h>
 
+#define STAT = 30;
+
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
+    USBInit();
+    pinMode(30, OUTPUT);
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(30, HIGH);
     delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(30, LOW);
     delay(1000);
 }
